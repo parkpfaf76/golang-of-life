3c58219216aa064b80186d3e39e7f51b1101bac8
Parker Pfaff <parkerp1@vt.edu>
2023-02-14 00:42:17 -0500

    Update

--- models/cellGrid2D.go
+++ models/cellGrid2D.go
@@ -1,19 +1,55 @@
 package models
 
+// grid origin from top-left (always select cell by row X col)
+// ex.
+//	 		0			  1			     2
+// 					|            |
+// 					|            |
+// 					|            |
+// 	0				|            |
+// 					|            |
+// 					|            |
+// 					|            |
+// 	   _____________|____________|_______________
+// 					|            |
+// 	   		 		|            |
+// 	 		    	|            |
+// 	1				|            |
+//  				|            |
+//  				|            |
+//  	____________|____________|_______________
+//  				|            |
+//  				|            |
+//  				|            |
+// 2				|            |
+//     		  		|            |
+// 	    		 	|            |
+// 		   		 	|            |
+
 // cell grid
 type cellGrid2D struct {
-	width int
-	height int
+	numColumns int
+	numRows int
 	cells [][]cell
 }
 
-func newCellGrid2D(width int, height int) cellGrid2D {
-	cells := [width][height]cell
-	g := cellGrid2D{width, height, cells, totalNumberAlive, totalNumberDeadCells}
+
+func newCellGrid2D(numRows int, numColumns int) cellGrid2D {
+	cells := [numRows][numColumns]cell{}
+
+	for _, array := range cells {
+        for j := range cellRow {
+			cells = append(
+				cells,
+				newCell(newCellRules(new int[]{2,3}, 3)) // hard code rules in each cell for now (2 or 3 neighbors to stay alive, 3 neighbors to spawn)
+        }
+    }
+
+	g := cellGrid2D{numRows, numColumns, cells}
 	return g
 }
 
 // retrive cell
-func (c cellGrid2D) getCell(posX int, posY int) cell {
-	return g.cells[posX][posY]
+func (c cellGrid2D) getCell(rowIdx int, colIdx int) cell {
+	return g.cells[rowIdx][colIdx]
 }
